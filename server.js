41cb6fa2166bd99074a82b0398b93906d2a3e20d
uniza.lin <unizalin@gmail.com>
2022-02-21 16:50:39 +0800

    add reaquest url methods different page

--- server.js
+++ server.js
@@ -9,10 +9,20 @@ var http = require("http");
 const requestListener = (request , response) =>{
   console.log(request.url)
   console.log(request.method)
-
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World !");
-  response.end();
+  const header =  {"Content-Type": "text/plain"}
+  if (request.url == '/' && request.method == 'GET'){
+    response.writeHead(200, header);
+    response.write("Hello World !");
+    response.end();
+  }else if(request.url == '/' && request.method == 'DELETE'){
+    response.writeHead(200, header);
+    response.write("Delete!");
+    response.end();
+  }else {
+    response.writeHead(200, header);
+    response.write("ERROR 404 !");
+    response.end();
+  }
 }
 const server = http.createServer(requestListener)
 server.listen(3005)
