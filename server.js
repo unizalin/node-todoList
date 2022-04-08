9aca26a99f8bba028f362199ddeb697ec9b15b02
Uniza Lin <unizalin@UnizadeiMac.local>
2022-04-08 13:47:08 +0800

    add server.js

--- server.js
+++ server.js
@@ -5,23 +5,33 @@ var http = require("http");
 //     response.write("Hello World");
 //     response.end();
 // }).listen(8888);
-
+console.log('test')
 const requestListener = (request , response) =>{
   console.log(request.url)
   console.log(request.method)
-  const header =  {"Content-Type": "text/plain"}
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  }
   if (request.url == '/' && request.method == 'GET'){
-    response.writeHead(200, header);
-    response.write("Hello World !");
+    response.writeHead(200, headers);
+    response.write(JSON.stringify({
+      "status":"success",
+      "data":[]
+    }));
     response.end();
   }else if(request.url == '/' && request.method == 'DELETE'){
-    response.writeHead(200, header);
+    response.writeHead(200, headers);
     response.write("Delete!");
     response.end();
   }else {
-    response.writeHead(200, header);
-    response.write("ERROR 404 !");
-    response.end();
+    response.writeHead(404, headers);
+    response.write(JSON.stringify({
+      "status":"false",
+      "message":"無此網站"
+    }));    response.end();
   }
 }
 const server = http.createServer(requestListener)
